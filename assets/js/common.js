d2075db65fa03cdfd7e6788185a7e0bfa75aa124
dworker <dworker@gmail.com>
2022-05-03 00:36:54 +0600

    Nav-mobile all screens

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,5 +2,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
   $(".hamburger-wrapper").on("click", function (e) {
     const th = $(this);
     $(th).toggleClass("active");
+
+    if ($(th).hasClass("active")) {
+      $(".nav-mobile").addClass("active");
+    } else {
+      $(".nav-mobile").removeClass("active");
+    }
   });
 });
